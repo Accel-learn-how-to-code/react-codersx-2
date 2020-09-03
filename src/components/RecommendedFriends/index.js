6ed44e33059a2303ac69855deaa2a0caf85f04be
Accel <cyberpunk2076@gmail.com>
2020-09-03 11:21:42 +0700

    bai 11

--- src/components/RecommendedFriends/index.js
+++ src/components/RecommendedFriends/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './RecommendedFriends.css';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import FollowFriendListItem from '../FollowFriendListItem/index';
 
 function RecommendedFriends(props){
     return(
         <div className="RecommendedFriends-container">
             <div className="RecommendedFriends">
+            <div class="Recommended-Friends-Header-Container">
                 <h1>Accounts</h1>
+                <div class="bell">
+                    <FontAwesomeIcon icon={faBell}/>
+                    {
+                        props.friends.length ? <div class="notic">{props.friends.length}</div> : null
+                    }
+                </div>
+            </div>
                 <hr />
                 {
                     props.friends.map((x, index) => <FollowFriendListItem key={index} id={index} name={x.name} email={x.email} icon={faTrashAlt}/>)
