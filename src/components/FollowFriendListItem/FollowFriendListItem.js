eabcce772f2fd2fd38cb2f52c9bc52dfe3ca8568
Accel <cyberpunk2076@gmail.com>
2020-09-09 21:18:23 +0700

    Bai 19

--- src/components/FollowFriendListItem/FollowFriendListItem.js
+++ src/components/FollowFriendListItem/FollowFriendListItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './FollowFriendListItem.css';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import './FollowFriendListItem.css';
+
+
 function FollowFriendListItem(props){
+    const {user, icon} = props;
     let itemName = 'items'
-    let index = Number(props.id);
+    let index = Number(user.id);
     if(index === 0 || index % 2 === 0){
         itemName += ' even';
     }
@@ -14,14 +18,23 @@ function FollowFriendListItem(props){
                 <div className="image"></div>
             </div>
             <div className="title">
-                <h2 className="name">{props.name}</h2>
-                <p className="email">{props.email}</p>
+                <h2 className="name">{user.name}</h2>
+                <p className="email">{user.email}</p>
             </div>
             <div className="icon-container">
-                <FontAwesomeIcon icon={props.icon} />
+                <FontAwesomeIcon icon={icon} />
             </div>
         </div>
     );
 }
+
+FollowFriendListItem.propTypes = {
+    user: PropTypes.shape({
+        name: PropTypes.string,
+        email: PropTypes.string,
+        icon: PropTypes.object
+    }),
+    icon: PropTypes.object
+};
 
 export default FollowFriendListItem
--- src/components/RecommendedFriends/RecommendedFriends.js
+++ src/components/RecommendedFriends/RecommendedFriends.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './RecommendedFriends.css';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
@@ -6,25 +7,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import FollowFriendListItem from '../FollowFriendListItem/FollowFriendListItem';
 
 function RecommendedFriends(props){
+    const {friends} = props;
     return(
         <div className="RecommendedFriends-container">
             <div className="RecommendedFriends">
-            <div class="Recommended-Friends-Header-Container">
+            <div className="Recommended-Friends-Header-Container">
                 <h1>Accounts</h1>
-                <div class="bell">
+                <div className="bell">
                     <FontAwesomeIcon icon={faBell}/>
                     {
-                        props.friends.length ? <div class="notic">{props.friends.length}</div> : null
+                        friends.length ? <div className="notic">{friends.length}</div> : null
                     }
                 </div>
             </div>
                 <hr />
                 {
-                    props.friends.map((x, index) => <FollowFriendListItem key={index} id={index} name={x.name} email={x.email} icon={faTrashAlt}/>)
+                    friends.map((x, index) => <FollowFriendListItem key={index} id={index} user={x} icon={faTrashAlt}/>)
                 }
             </div>
         </div>
     );
 }
+
+RecommendedFriends.propTypes = {
+    friends: PropTypes.array
+};
 
 export default RecommendedFriends
